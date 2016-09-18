089739b60f86a6351e8f1d795dbaebfb54502587
zion <guzhaoyuan@126.com>
2016-09-18 14:04:08 +0800

    better install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
+#backup original .vim/ and .vimrc
+if [ -e ~/.vim ]
+then
+	mv .vim/ .vim_backup/
+	echo ".vim/ exists, backup .vim/ to .vim_backup"
+fi
+
+if [ -e .vimrc  ]
+then
+	mv .vimrc .vimrc_backup
+	echo ".vimrc exists, backup .vimrc to .vimrc_backup"
+fi
+
+
+#copy file to home directory
 cp -r .vim ~/
 cp .vimrc ~/
+
+#finish
+echo "install complete"
+vim
